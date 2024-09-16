cb79706c26bebd114ee056255c0eef2e7723d14d
Patrice Fortin <patricefortin.x@gmail.com>
2024-09-16 17:06:19 -0400

    release 1.0.0+4

--- scripts/run-build-release.sh
+++ scripts/run-build-release.sh
@@ -6,10 +6,16 @@ cd "$(dirname "$0")"
 cd ..
 
 VERSION=$(dart run lib/main_version.dart)
-TARGET="releases/s2m-mappemg-${VERSION}.apk"
+TARGET_APK="releases/s2m-mappemg-${VERSION}.apk"
+TARGET_AAB="releases/s2m-mappemg-${VERSION}.aab"
 
-if [ -f $TARGET ]; then
-    echo "Target $TARGET already exists. Please update version in lib/constants.dart"
+if [ -f $TARGET_APK ]; then
+    echo "Target $TARGET_APK already exists. Please update version in lib/constants.dart"
+    exit 1
+fi
+
+if [ -f $TARGET_AAB ]; then
+    echo "Target $TARGET_AAB already exists. Please update version in lib/constants.dart"
     exit 1
 fi
 
@@ -17,10 +23,12 @@ fi
 sed -i "s/^version:.*/version: ${VERSION}/" pubspec.yaml
 
 flutter build apk
-cp build/app/outputs/flutter-apk/app-release.apk $TARGET
+cp build/app/outputs/flutter-apk/app-release.apk $TARGET_APK
 
-#git add $TARGET $ANDROID_PROPERTIES
-git add $TARGET lib/constants.dart pubspec.yaml
+flutter build appbundle
+cp build/app/outputs/bundle/release/app-release.aab $TARGET_AAB
+
+git add $TARGET_APK $TARGET_AAB lib/constants.dart pubspec.yaml
 git status
 
 echo "## Run this to push"
